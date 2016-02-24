f2d9c95dd73ffabac137e86d6cb8993033bb5506
MBA <muhammadbilalansari@yahoo.com>
2016-02-24 23:03:54 +0500

    links utha rha hai

---
+++ WebCrawlerForm/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WebCrawlerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTB = new System.Windows.Forms.TextBox();
+            this.fetchBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.tbSvAdd = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // urlTB
+            // 
+            this.urlTB.Location = new System.Drawing.Point(175, 45);
+            this.urlTB.Name = "urlTB";
+            this.urlTB.Size = new System.Drawing.Size(385, 20);
+            this.urlTB.TabIndex = 0;
+            // 
+            // fetchBTN
+            // 
+            this.fetchBTN.Location = new System.Drawing.Point(566, 45);
+            this.fetchBTN.Name = "fetchBTN";
+            this.fetchBTN.Size = new System.Drawing.Size(75, 23);
+            this.fetchBTN.TabIndex = 1;
+            this.fetchBTN.Text = "Fetch";
+            this.fetchBTN.UseVisualStyleBackColor = true;
+            this.fetchBTN.Click += new System.EventHandler(this.fetchBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(46, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter URL/URI :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(286, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "WebCrawler";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(50, 83);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(86, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Proxy Server";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // tbSvAdd
+            // 
+            this.tbSvAdd.Enabled = false;
+            this.tbSvAdd.Location = new System.Drawing.Point(233, 83);
+            this.tbSvAdd.Name = "tbSvAdd";
+            this.tbSvAdd.Size = new System.Drawing.Size(177, 20);
+            this.tbSvAdd.TabIndex = 7;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Enabled = false;
+            this.tbPort.Location = new System.Drawing.Point(488, 84);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(100, 20);
+            this.tbPort.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(145, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Server Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(453, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Port:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(50, 140);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(591, 615);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 781);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbSvAdd);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fetchBTN);
+            this.Controls.Add(this.urlTB);
+            this.Name = "Form1";
+            this.Text = "WebCrawler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTB;
+        private System.Windows.Forms.Button fetchBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox tbSvAdd;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WebCrawlerForm/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WebCrawlerForm
+{
+    public partial class Form1 : Form
+    {
+        string contents = "";
+        List<string> linklist = new List<string>();
+        int start = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void fetchBTN_Click(object sender, EventArgs e)
+        {
+            WebRequest httpReq;
+            string contents = "";
+            List<string> linklist = new List<string>();
+            int start = 0;
+
+
+            if (urlTB.Text != string.Empty || urlTB.Text != "" && checkBox1.Checked == false)
+            {
+                try
+                {
+
+                    httpReq = HttpWebRequest.Create(urlTB.Text.ToString());
+                    WebResponse response = httpReq.GetResponse();
+                    Stream dataStream = response.GetResponseStream();
+                    using (StreamReader reader = new StreamReader(dataStream))
+                    {
+                        contents = reader.ReadToEnd();
+
+                    }
+
+                    do
+                    {
+                        listBox1.Items.Add(FindLink(contents, ref start));
+                    } while (start < contents.Length);
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            else if (urlTB.Text != string.Empty || urlTB.Text != "" && checkBox1.Checked == true)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlTB.Text);
+                WebProxy myproxy = new WebProxy(tbSvAdd.Text + ":" + tbPort, true);
+                myproxy.BypassProxyOnLocal = false;
+                request.Proxy = myproxy;
+                request.Method = "GET";
+                request.UserAgent = "Crawler";
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    contents = reader.ReadToEnd();
+                }
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (tbSvAdd.Enabled == false && tbPort.Enabled == false)
+            {
+                tbSvAdd.Enabled = true;
+                tbPort.Enabled = true;
+            }
+            else
+            {
+                tbSvAdd.Enabled = false;
+                tbPort.Enabled = false;
+            }
+        }
+
+
+        static string FindLink(string htmlstr, ref int startloc)
+        {
+            int i;
+            int start, end;
+            string uri = null;
+            i = htmlstr.IndexOf("href=\"http", startloc,
+            StringComparison.OrdinalIgnoreCase);
+            if (i != -1)
+            {
+                start = htmlstr.IndexOf('"', i) + 1;
+                end = htmlstr.IndexOf('"', start);
+                uri = htmlstr.Substring(start, end - start);
+                startloc = end;
+            }
+            return uri;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            WebRequest httpReq;
+            urlTB.Text = listBox1.SelectedItem.ToString();
+            try
+            {
+
+                httpReq = HttpWebRequest.Create(urlTB.Text.ToString());
+                WebResponse response = httpReq.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                using (StreamReader reader = new StreamReader(dataStream))
+                {
+                    contents = reader.ReadToEnd();
+
+                }
+
+                do
+                {
+                  listBox1.Items.Add(FindLink(contents, ref start));
+                } while (start < contents.Length);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
